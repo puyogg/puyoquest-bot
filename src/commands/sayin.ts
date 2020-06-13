410b7fff952581cbad42cca86dad387dca23b7fb
Phong Tran <s2@puyonexus.com>
2020-06-13 11:45:30 -0400

    Create sayin.ts

---
+++ src/commands/sayin.ts
@@ -0,0 +1,31 @@
+import * as path from 'path';
+import * as Discord from 'discord.js';
+import { Command } from '../command-info';
+
+// Retrieve command name from filename
+const name = path.parse(__filename).name;
+
+const command: Command = {
+  name: name, // card
+  usage: ['sayin <channel-tag> <message>'],
+  description: 'Look up a card on the Puyo Nexus Wiki',
+  args: true,
+  aliases: [],
+  category: ['puyoquest'],
+  async execute(message: Discord.Message, args: string[]): Promise<void> {
+    // Only admins can use this command
+    if (!message.member?.hasPermission('ADMINISTRATOR')) {
+      return;
+    }
+
+    if (args.length < 2) return;
+
+    const channelID = args[0].slice(2, args[0].length - 1);
+    const channel = (await message.client.channels.fetch(channelID)) as Discord.TextChannel;
+    const msg = args.slice(1).join(' ');
+
+    channel.send(msg);
+  },
+};
+
+export default command;
