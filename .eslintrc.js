c9ea8eb95cd4b497087e1feff1fed004b13e6569
Phong Tran <s2@puyonexus.com>
2020-06-16 22:27:11 -0400

    annoying EOL stuff; README update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,11 @@ module.exports = {
   },
   extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   rules: {
-    'linebreak-style': ['error', 'unix'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
